8a5557af8673c49b74fc198dfa42e3ea533c0907
Library-fox <70788864+Library-fox@users.noreply.github.com>
2021-02-08 22:13:36 -0800

    Update coinflip.py

--- coinflip.py
+++ coinflip.py
@@ -1,7 +1,6 @@
 '''
-version 2.6
-Minor changes to a bunch things
-no change in overall opperation or function
+version 3.1
+now writes output as csv files instead of plaintext
 '''
 from random import randint
 import time
@@ -16,9 +15,10 @@ filecounter=0
 filename=''
 while True:
     try:
-        filename='log-folder\logfile{}.TXT'.format(filecounter)
+        filename='log-folder\logfile{}.csv'.format(filecounter)
         with open(filename,'x') as file:
-            file.write("New run started on: {}\nStreak target: {}\nNumber of cycles: {}\n\n".format(time.asctime(),streakGoal,goal))
+            file.write("New run started on: {}".format(time.asctime()))
+            file.write("cycle,flips,heads,tails,time in microseconds\n")
         break
     except FileExistsError:
         filecounter+=1
@@ -38,7 +38,7 @@ while goal>counter:
     streakTails=0
     streakHeadsRecord=0
     streakTailsRecord=0 
-    startTime=time.time()*1000
+    startTime=time.time()*1000000
 
     while True:
         if streakGoal <= streakHeadsRecord:
@@ -57,10 +57,15 @@ while goal>counter:
             streakHeads=0
             if streakTails > streakTailsRecord:
                 streakTailsRecord=streakTails
-    finalTime=(time.time()*1000)-startTime
+    finalTime=(time.time()*1000000)-startTime
     with open(filename, 'a') as file:
-        file.write("cycle: {}\nflips: {}\nheads: {}\ntails: {}\ntime: {} millisecond(s)\n\n".format(counter,totalflips,streakHeadsRecord,streakTailsRecord,finalTime))
+        file.write("{},{},{},{},{}\n".format(counter,totalflips,streakHeadsRecord,streakTailsRecord,finalTime))
 
 with open(filename, 'a') as file:
-    file.write("Total run time: {} millisecond(s)\n".format((time.time()-absolutetime)*1000))
+    goal1=goal+1
+    file.write("Final,=AVERAGE(B2:B{}),=AVERAGE(C2:C{}),=AVERAGE(D2:D{}),{}\n".format(goal1,goal1,goal1,(time.time()-absolutetime)*1000000))
+    file.write('MAD,=AVEDEV(B2:B{}),=AVEDEV(C2:C{}),=AVEDEV(D2:D{}),\n'.format(goal1,goal1,goal1))
+    file.write('MIN,=MIN(B2:B{}),=MIN(C2:C{}),=MIN(D2:D{}),\n'.format(goal1,goal1,goal1))
+    file.write('MAX,=MAX(B2:B{})\n'.format(goal1))
 print("\nResults are in logfile{}".format(filecounter))
+
